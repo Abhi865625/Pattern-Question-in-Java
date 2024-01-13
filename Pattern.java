2949083f7237117cedbd53b02533a4572ab9e6c8
Abhishek Kumar Mehta <93569162+Abhi865625@users.noreply.github.com>
2024-01-13 20:08:10 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -364,10 +364,10 @@ System.out.println();
 
 
     for (int i = 0; i <=n; i++) {
-        int number = 1;
+        int number2 = 1;
         for (int j = 0; j <= i; j++) {
-            System.out.print(number + " ");
-            number = number * (i - j) / (j + 1);
+            System.out.print(number2 + " ");
+            number2 = number2 * (i - j) / (j + 1);
         }
         System.out.println();
     }
