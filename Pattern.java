5c42d7f4c4b1359cce18f3594753d292b7895b3a
Abhishek Kumar Mehta <93569162+Abhi865625@users.noreply.github.com>
2024-01-14 09:36:09 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -391,6 +391,57 @@ System.out.println();
         }
         System.out.println();
     }
+
+    System.out.println();
+    System.out.println();
+
+
+    for(int i=1; i<=n;i++){
+            for(int j=1; j<=i;j++){
+                if(j == 1 || i==j){
+                    System.out.print("*");  
+                }else{
+                    System.out.print(" "); 
+                }
+            }for(int j=1;j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i;j++){
+                if(j == 1  || i==j){
+                    System.out.print("*");  
+                }else{
+                    System.out.print(" "); 
+                }   
+        }
+        System.out.println();
+    }
+
+        for(int i=n-1; i>=1;i--){
+            for(int j=1; j<=i;j++){
+                if(j == 1  || i == n || i==j){
+                    System.out.print("*");  
+                }else{
+                    System.out.print(" "); 
+                }
+            }for(int j=1;j<=2*(n-i); j++){
+                System.out.print(" ");
+         }
+            for(int j=1; j<=i;j++){
+                if(j == 1  || i == n || i==j){
+                System.out.print("*");  
+                }else{
+                System.out.print(" "); 
+             }   
+        }
+        System.out.println();
+    }
+
+        System.out.println();
+        System.out.println();
+
+
+
+        
  
 
     }
